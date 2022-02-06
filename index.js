a427d3cd6b7a8cb364542ba343ec8af6a73fdbf0
amr.metwally <magdyamr542@gmail.com>
2022-02-06 23:20:40 +0100

    Added view with req headers

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.set('view engine', 'pug')
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  const headers = req.headers;
+  const ips = req.ips;
+  res.render('index', { title: 'Nginx LB',  headers : JSON.stringify(headers) , ips : JSON.stringify(ips) })
+});
+
+app.listen(port , () => {
+  console.log('Server started at http://localhost:' + port);
+});
+
+
